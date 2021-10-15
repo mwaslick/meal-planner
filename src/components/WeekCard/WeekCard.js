97552f2cc19d97294b4dedadee7476dee2122bc0
Maria Waslick <mariawaslick@gmail.com>
2021-10-15 14:20:56 -0400

    added correct dates to project

--- src/components/WeekCard/WeekCard.js
+++ src/components/WeekCard/WeekCard.js
@@ -1,16 +1,25 @@
 import React, { useState } from 'react';
 import { Container, Row, Col, Card, ListGroup, ListGroupItem } from 'react-bootstrap';
+import DayJS from 'react-dayjs';
 
 export default function WeekCard(props) {
 
-  return (
-        <Card>
-            <Card.Header>{props.weekday}</Card.Header>
-            <ListGroup variant="flush">
-                <ListGroup.Item>Breakfast</ListGroup.Item>
-                <ListGroup.Item>Lunch</ListGroup.Item>
-                <ListGroup.Item>Dinner</ListGroup.Item>
-            </ListGroup>
-        </Card>
-  );
+    let isToday = props.today;
+
+        return (
+            <Card>
+                {isToday? <Card.Header>
+                   <b> {props.weekday} <br></br> {props.date} </b>
+                </Card.Header> : 
+                <Card.Header>
+                    {props.weekday} <br></br> {props.date}   
+                </Card.Header>}
+                
+                <ListGroup variant="flush">
+                    <ListGroup.Item>Breakfast</ListGroup.Item>
+                    <ListGroup.Item>Lunch</ListGroup.Item>
+                    <ListGroup.Item>Dinner</ListGroup.Item>
+                </ListGroup>
+            </Card>
+      );
 }
